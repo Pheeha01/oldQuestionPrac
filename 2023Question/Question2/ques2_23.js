761b41cbec70bc94e2129a63aec459da5dd04e3a
T.Mabotja <mabatshegofatso842@gmail.com>
2026-04-01 03:39:59 +0200

    my code is very repetitive, and needs more cleaning with more knowledge gained

---
+++ 2023Question/Question2/ques2_23.js
@@ -0,0 +1,96 @@
+let testMarks= document.getElementById("testResults");
+let calculateBtn= document.getElementById("computeBtn");
+let clearBtn=document.getElementById("clearFields");
+let perc1 = document.getElementById("test1Perc")
+let perc2 = document.getElementById("test2Perc")
+let perc3 = document.getElementById("test3Perc")
+let perc4 = document.getElementById("test4Perc")
+
+testMarks.addEventListener('input', (e)=>{
+    if(isNaN(e.target.value)){
+            alert("enter a valid number and it should be greater than or equals to 0")
+            e.target.value =""
+    }
+    else{
+        switch (e.target.id){
+            case "test1":
+                if(e.target.value > 50){
+                    alert("Aowa monna, moes you know that your test was out of 50, so how did you go above that?")
+                }
+                else{
+                    calculateBtn.addEventListener('click', ()=>{
+                        let percentage= Math.ceil((Number(e.target.value)/50)*100)
+                        perc1.textContent = `${percentage}`
+                        if(percentage>50){
+                            perc1.classList.add("pass")
+                        }
+                        else{
+                            perc1.classList.add("fail")
+                        }   
+                    })
+                }
+                break;
+            case "test2":
+                if(e.target.value > 60){
+                    alert("Aowa monna, moes you know that your test was out of 60, so how did you go above that?")
+                }
+                else{
+                    calculateBtn.addEventListener('click', ()=>{
+                        let percentage= Math.ceil((Number(e.target.value)/60)*100)
+                        perc2.textContent = `${percentage}`
+                        if(percentage>50){
+                            perc2.classList.add("pass")
+                        }
+                        else{
+                            perc2.classList.add("fail")
+                        }
+                    })               
+                }
+                break;
+            case "test3":
+                if(e.target.value > 60){
+                    alert("Aowa monna, moes you know that your test was out of 60, so how did you go above that?")
+                }
+                else{
+                    calculateBtn.addEventListener('click', ()=>{
+                        let percentage= Math.ceil((Number(e.target.value)/60)*100)
+                        perc3.textContent = `${percentage}`
+                        if(percentage>50){
+                            perc3.classList.add("pass")
+                        }
+                        else{
+                            perc3.classList.add("fail")
+                        }
+                    })                
+                }
+                break;
+            case "test4":
+                if(e.target.value > 50){
+                    alert("Aowa monna, moes you know that your test was out of 50, so how did you go above that?")
+                }
+                else{
+                    calculateBtn.addEventListener('click', ()=>{
+                        let percentage= Math.ceil((Number(e.target.value)/50)*100)
+                        perc4.textContent = `${percentage}`
+                        if(percentage>50){
+                            perc4.classList.add("pass")
+                        }
+                        else{
+                            perc4.classList.add("fail")
+                        }
+                    })                
+                }
+                break;
+        }
+    }
+    clearBtn.addEventListener("click", () => {
+        document.querySelectorAll("#testResults input").forEach(input => {
+            input.value = "";
+        });
+        document.querySelectorAll(".pass, .fail").forEach(el => {
+            el.textContent = "";
+            el.classList.remove("pass", "fail");
+        });
+    });
+})
+
