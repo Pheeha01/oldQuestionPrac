099096d6bfb13f7958c227e0bf6d1d7bcb6db520
T.Mabotja <mabatshegofatso842@gmail.com>
2026-04-01 23:33:13 +0200

    still trying to refine my skills with regards to the mandela

---
+++ 2023Question/Question3/ques3_23.js
@@ -0,0 +1,28 @@
+let pics = document.getElementById("pics")
+let favoriteSection = document.getElementById("favorites")
+let actions= document.getElementById("actions")
+
+pics.addEventListener('click', (e)=>{
+    if(e.target.tagName.toLowerCase()!=="img"){return}
+
+    if(!favoriteSection.contains(e.target)){
+        favoriteSection.appendChild(e.target)
+        e.target.classList.add("moved")
+        let li = document.createElement("li")
+        li.textContent= `${e.target.src.slice(-19)} was moved to favorites`;
+        li.classList.add("whatF")
+        actions.append(li)
+    }
+})
+favoriteSection.addEventListener('click', (e)=>{
+    if(e.target.tagName.toLowerCase()!=="img"){return}
+
+    if(favoriteSection.contains(e.target)){
+        pics.appendChild(e.target)
+        e.target.classList.remove("moved")
+        let li = document.createElement("li")
+        li.textContent= `${e.target.src.slice(-19)} was moved back to album`;
+        li.classList.add("whatA")
+        actions.append(li)
+    }
+})
